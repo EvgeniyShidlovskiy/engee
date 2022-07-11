3512ec912d183b8063e69d123682a46d95de68ab
EvgeniyShidlovskiy <101859241+EvgeniyShidlovskiy@users.noreply.github.com>
2022-07-11 14:27:26 +0300

    Add files via upload

--- Histogram.jl
+++ Histogram.jl
@@ -1,4 +1,4 @@
-using OffsetArrays
+
 mutable struct Histogram
   find_hist_over::String
   bins::Int64
@@ -8,6 +8,7 @@ mutable struct Histogram
   running:: Bool
   reset_port:: String
   y::Array{Float64}
+  ye::Array{Float64}
 
   function Histogram(find_hist_over::String,bins::Int64,low_lim::Int64,up_lim::Int64)
   if find_hist_over != "Entire input" && find_hist_over != "Each column"
@@ -26,7 +27,7 @@ mutable struct Histogram
       if find_hist_over != "Entire input" && find_hist_over != "Each column"
         error("Поиск гистограммы должен быть по методу Entire input или Each column")
       end
-         new(find_hist_over,bins,low_lim,up_lim,normalized,running,reset_port,zeros(bins))
+         new(find_hist_over,bins,low_lim,up_lim,normalized,running,reset_port,zeros(bins),zeros(bins,3))
   end  
 end
 
@@ -202,11 +203,71 @@ function step(var_hist::Histogram,u,rst)
          i=i+1;
      end
     end
+  elseif var_hist.find_hist_over == "Each column" && var_hist.normalized == false && var_hist.running == true && var_hist.reset_port == "None-zero sample" 
+  
+    if rst != 0 
+     i = -(size(u,1)-1)
+    idxOutO = 0;
+    for j = 0:size(u,2)-1
+     
+      idxIn = i + (size(u,1)-1);
+      for k = 0:size(u,1)-1
+        
+           if u[idxIn+1] <= var_hist.low_lim
+            u0 = 0;
+           elseif u[idxIn+1] > var_hist.up_lim
+             u0 = var_hist.bins-1;
+           else 
+            r= var_hist.bins/(var_hist.up_lim-var_hist.low_lim)
+            u0::Int = ceil((u[idxIn+1] - var_hist.low_lim) *  r - 1.0);
+            if (u0 > var_hist.bins-1) 
+              u0 = var_hist.bins-1;
+        end
+           end
+
+          u0 = u0 + idxOutO
+          var_hist.ye[u0+1]=var_hist.ye[u0+1]+1;
+          idxIn=idxIn+1;
+        end
+  
+      i = i + size(u,1);
+      idxOutO =idxOutO + var_hist.bins;
+   end
+  else 
+    i = -(size(u,1)-1)
+    idxOutO = 0;
+    for j = 0:size(u,2)-1
+     
+      idxIn = i + (size(u,1)-1);
+      for k = 0:size(u,1)-1
+        
+           if u[idxIn+1] <= var_hist.low_lim
+            u0 = 0;
+           elseif u[idxIn+1] > var_hist.up_lim
+             u0 = var_hist.bins-1;
+           else 
+            r= var_hist.bins/(var_hist.up_lim-var_hist.low_lim)
+            u0::Int = ceil((u[idxIn+1] - var_hist.low_lim) *  r - 1.0);
+            if (u0 > var_hist.bins-1) 
+              u0 = var_hist.bins-1;
+        end
+           end
+
+          u0 = u0 + idxOutO
+          var_hist.ye[u0+1]=var_hist.ye[u0+1]+1;
+          idxIn=idxIn+1;
+        end
+  
+      i = i + size(u,1);
+      idxOutO =idxOutO + var_hist.bins;
+   end
+  end  
   end
-return var_hist.y
+return var_hist.ye
 end
 
-var_hist=Histogram("Entire input",7,1,10,false,true,"None-zero sample")
+var_hist=Histogram("Each column",7,1,10,false,true,"None-zero sample")
+step(var_hist,[1 2 -2; 4 83 6; 4 100 6;4 83 6;4 -80 6;4 83 0;4 83 6;2 83 6;1 83 6],0)
 step(var_hist,[1 2 -2; 4 83 6; 4 100 6;4 83 6;4 -80 6;4 83 0;4 83 6;2 83 6;1 83 6],0)
 
 
