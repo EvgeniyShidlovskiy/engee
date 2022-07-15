f382515619f4bfac5e955bd3219dcbc2f9e301e4
EvgeniyShidlovskiy <101859241+EvgeniyShidlovskiy@users.noreply.github.com>
2022-07-15 11:54:50 +0300

    Add files via upload

--- Histogram.jl
+++ Histogram.jl
@@ -13,7 +13,7 @@ mutable struct Histogram
   Histogram_Iteration::Float64
   prv_rst::Int64
   
-
+#need add running error
   function Histogram(find_hist_over::String,bins::Int64,low_lim::Int64,up_lim::Int64)
   if find_hist_over != "Entire input" && find_hist_over != "Each column"
     error("Поиск гистограммы должен быть по методу Entire input или Each column")
@@ -365,9 +365,17 @@ elseif var_hist.find_hist_over == "Each column" && var_hist.normalized == true &
   return var_hist.ye
 elseif var_hist.find_hist_over == "Entire input" && var_hist.normalized == false && var_hist.running == true 
   
-  if (var_hist.reset_port == "Either edge" && ((var_hist.prv_rst == 0 && rst !=0) ||  (var_hist.prv_rst != 0 && rst == 0))) ||
-     (var_hist.reset_port == "Rising edge" && var_hist.prv_rst == 0 && rst != 0) ||
-      (var_hist.reset_port == "Falling edge"  &&var_hist.prv_rst != 0 && rst == 0) 
+  if (var_hist.reset_port == "Either edge" &&
+    ((var_hist.prv_rst == 0 && rst !=0) || (var_hist.prv_rst != 0 && rst == 0))) ||
+  
+    (var_hist.reset_port == "Falling edge"  &&
+    var_hist.prv_rst != 0 && rst == 0) ||
+  
+    (var_hist.reset_port == "Rising edge" &&
+    var_hist.prv_rst == 0 && rst != 0) 
+    
+    
+  
 
     for i = 0:var_hist.bins-1
       var_hist.y[i+1] = 0.0;
@@ -413,26 +421,193 @@ elseif var_hist.find_hist_over == "Entire input" && var_hist.normalized == false
   end
   var_hist.prv_rst = rst
 return var_hist.y   
+elseif var_hist.find_hist_over == "Each column" && var_hist.normalized == false && var_hist.running == true 
+  
+  if (var_hist.reset_port == "Either edge" &&
+    ((var_hist.prv_rst == 0 && rst !=0) || (var_hist.prv_rst != 0 && rst == 0))) ||
+  
+    (var_hist.reset_port == "Falling edge"  &&
+    var_hist.prv_rst != 0 && rst == 0) ||
+  
+    (var_hist.reset_port == "Rising edge" &&
+    (var_hist.prv_rst == 0 && rst != 0)) 
+
+    var_hist.ye = zeros(var_hist.bins,size(u,2))
+
+   i = -(size(u,1)-1)
+  idxOutO = 0;
+  for j = 0:size(u,2)-1
+   
+    idxIn = i + (size(u,1)-1);
+    for k = 0:size(u,1)-1
+      
+         if u[idxIn+1] <= var_hist.low_lim
+          u0 = 0;
+         elseif u[idxIn+1] > var_hist.up_lim
+           u0 = var_hist.bins-1;
+         else 
+          r= var_hist.bins/(var_hist.up_lim-var_hist.low_lim)
+          u0::Int = ceil((u[idxIn+1] - var_hist.low_lim) *  r - 1.0);
+          if (u0 > var_hist.bins-1) 
+            u0 = var_hist.bins-1;
+          end
+         end
+
+        u0 = u0 + idxOutO
+        var_hist.ye[u0+1]=var_hist.ye[u0+1]+1;
+        idxIn=idxIn+1;
+      end
+
+    i = i + size(u,1);
+    idxOutO =idxOutO + var_hist.bins;
+ end
+else 
+  i = -(size(u,1)-1)
+  idxOutO = 0;
+  for j = 0:size(u,2)-1
+   
+    idxIn = i + (size(u,1)-1);
+    for k = 0:size(u,1)-1
+      
+         if u[idxIn+1] <= var_hist.low_lim
+          u0 = 0;
+         elseif u[idxIn+1] > var_hist.up_lim
+           u0 = var_hist.bins-1;
+         else 
+          r= var_hist.bins/(var_hist.up_lim-var_hist.low_lim)
+          u0::Int = ceil((u[idxIn+1] - var_hist.low_lim) *  r - 1.0);
+          if (u0 > var_hist.bins-1) 
+            u0 = var_hist.bins-1;
+      end
+         end
+
+        u0 = u0 + idxOutO
+        var_hist.ye[u0+1]=var_hist.ye[u0+1]+1;
+        idxIn=idxIn+1;
+      end
+
+    i = i + size(u,1);
+    idxOutO =idxOutO + var_hist.bins;
+ end
+end 
+var_hist.prv_rst = rst
+return var_hist.ye
+
+elseif var_hist.find_hist_over == "Entire input" && var_hist.normalized == true && var_hist.running == true 
+
+
+  if (var_hist.reset_port == "Either edge" &&
+    ((var_hist.prv_rst == 0 && rst !=0) || (var_hist.prv_rst != 0 && rst == 0))) ||
+  
+    (var_hist.reset_port == "Falling edge"  &&
+    var_hist.prv_rst != 0 && rst == 0) ||
+  
+    (var_hist.reset_port == "Rising edge" &&
+    (var_hist.prv_rst == 0 && rst != 0)) 
+    for i = 0: var_hist.bins-1
+      var_hist.Histogram_BinCount[i+1] = 0.0;
+    end
+
+    var_hist.Histogram_Iteration = 0.0;
   end
+
+  i = 0;
+  for k = 0:size(u,1)*size(u,2)-1
+   
+      
+     if u[i+1] <= var_hist.low_lim
+           u0 = 0;
+          elseif u[i+1] > var_hist.up_lim
+           u0 = var_hist.bins-1;
+          else 
+         r=var_hist.bins/(var_hist.up_lim-var_hist.low_lim)
+           u0::Int = ceil((u[i+1] - var_hist.low_lim) * r - 1.0);
+           if (u0 > var_hist.bins-1) 
+             u0 = var_hist.bins-1;
+           end
+         end
+
+         var_hist.Histogram_BinCount[u0+1]=var_hist.Histogram_BinCount[u0+1]+1;
+  
+
+    i=i+1;
+  end
+
+  scale = 1.0 / ((var_hist.Histogram_Iteration .+ 1.0) .* size(u,1)*size(u,2));
+  var_hist.Histogram_Iteration=var_hist.Histogram_Iteration.+1;
+  
+  for i = 0:1:var_hist.bins-3
+    var_hist.y[i+1] = var_hist.Histogram_BinCount[i+1] * scale;
+  end
+
+  for i = var_hist.bins-2:var_hist.bins-1
+     var_hist.y[i+1] = var_hist.Histogram_BinCount[i+1] * scale;
+  end
+var_hist.prv_rst = rst
+return var_hist.y
+
+elseif var_hist.find_hist_over == "Each column" && var_hist.normalized == true && var_hist.running == true
+  if (var_hist.reset_port == "Either edge" &&
+    ((var_hist.prv_rst == 0 && rst !=0) || (var_hist.prv_rst != 0 && rst == 0))) ||
+  
+    (var_hist.reset_port == "Falling edge"  &&
+    var_hist.prv_rst != 0 && rst == 0) ||
+  
+    (var_hist.reset_port == "Rising edge" &&
+    var_hist.prv_rst == 0 && rst != 0)  
+
+    var_hist.Histogram_Iteration = 0.0;
+  end
+
+ i = -(size(u,1)-1);
+idxOutO = 0;
+for j = 0:size(u,2)-1
+  
+   idxIn = i + (size(u,1)-1);
+    for k = 0:size(u,1)-1
+  
+       if u[idxIn+1] <= var_hist.low_lim
+          u0 = 0;
+         elseif u[idxIn+1] > var_hist.up_lim
+           u0 = var_hist.bins-1;
+         else 
+          r= var_hist.bins/(var_hist.up_lim-var_hist.low_lim)
+          u0::Int = ceil((u[idxIn+1] - var_hist.low_lim) *  r - 1.0);
+          if (u0 > var_hist.bins-1) 
+            u0 = var_hist.bins-1;
+    end
+         end
+
+      u0 = u0 + idxOutO
+      var_hist.Histogram_BinCount[u0+1]=var_hist.Histogram_BinCount[u0+1]+1;
+  
+
+    idxIn=idxIn+1;
+  end
+
+   i = i + size(u,1);
+   idxOutO =idxOutO + var_hist.bins;
+end
+
+ scale = 1.0 / ((var_hist.Histogram_Iteration .+ 1.0) .* size(u,1));
+  var_hist.Histogram_Iteration=var_hist.Histogram_Iteration.+1;
+for i = 0:1:(var_hist.bins*size(u,2))-3
+ var_hist.ye[i+1] = var_hist.Histogram_BinCount[i+1] * scale;
+end
+
+for i = (var_hist.bins*size(u,2))-2:(var_hist.bins*size(u,2))-1
+  var_hist.ye[i+1] = var_hist.Histogram_BinCount[i+1] * scale;
+end
+var_hist.prv_rst = rst
+return var_hist.ye
+end
 
 end
 
 
-var_hist=Histogram("Entire input",7,1,10,false,true,"Falling edge")
+var_hist=Histogram("Entire input",7,1,10,true,true,"None-zero sample")
 setup(var_hist,[1 2 -2; 4 83 6; 4 100 6;4 83 6;4 -80 6;4 83 0;4 83 6;2 83 6;1 83 6])
 step(var_hist,[1 2 -2; 4 83 6; 4 100 6;4 83 6;4 -80 6;4 83 0;4 83 6;2 83 6;1 83 6],0)
 step(var_hist,[1 2 -2; 4 83 6; 4 100 6;4 83 6;4 -80 6;4 83 0;4 83 6;2 83 6;1 83 6],1)
+step(var_hist,[1 2 -2; 4 83 6; 4 100 6;4 83 6;4 -80 6;4 83 0;4 83 6;2 83 6;1 83 6],1)
 step(var_hist,[1 2 -2; 4 83 6; 4 100 6;4 83 6;4 -80 6;4 83 0;4 83 6;2 83 6;1 83 6],0)
-
-
-
-
-
-
-
-
-
-
-
-
-
